767783422f8a41a1a4fd3d7df297550f33c00649
Huruba Bianca <biancahuruba@yahoo.com>
2016-12-15 08:14:54 +0200

    C9 fara update db

--- MVCCurs8/MVCCurs8/Models/ProjectEvaluation.cs
+++ MVCCurs8/MVCCurs8/Models/ProjectEvaluation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,10 +10,15 @@ namespace MVCCurs8.Models
 
     {
         public int Id { get; set; }
+        [StringLength(1000)]
+        [Required]
         public string Name { get; set; }
 
         public string City { get; set; }
         public string Country { get; set; }
+
+        [Range(1,10)]
+        [Required]
         public double Rating { get; set; }
     }
 }
